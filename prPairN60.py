f7e37da0a4c3ed17ba852e5698e5a9559d6a1e4e
vharavu <v.haravu@gmail.com>
2016-11-10 14:47:52 -0800

    Improved performance

--- prPairN60.py
+++ prPairN60.py
@@ -26,20 +26,28 @@ def isItPrime(numList):
         for y in range(2, x):
             if (x % y) == 0:
                 isPrime = 0
+                break
         pList.append(isPrime)
     return pList
 
-print findPrime(800, 900)
-
+#print findPrime(673, 20000)
+samplePrList = findPrime(673, 20000)
+print len(samplePrList)
 fourList = ["3", "7", "109", "673"]
 combPlist = []
-for x in range(0, len(fourList)):
-    for y in range(0, len(fourList)):
-        if (x != y):
-            combPlist.append(int(fourList[x] + fourList[y]))
-
-print combPlist
-print isItPrime(combPlist)
-if 0 not in combPlist:
-    print "all are primes!!"
-
+for n in range(0, 2000):
+    print n
+    combPlist = []
+    fourList.append(str(samplePrList[n]))
+    for x in range(0, len(fourList)-1):
+        combPlist.append(int(fourList[x] + fourList[4]))
+        combPlist.append(int(fourList[4] + fourList[x]))
+    #print combPlist
+    #print isItPrime(combPlist)
+    resultList = isItPrime(combPlist)
+    if 0 in resultList:
+        del fourList[-1]
+    else:
+        print "all are primes!!"
+        print fourList
+        break
